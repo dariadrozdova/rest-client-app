96fe75ec01c46d4296fa3c6ca8618c283ee09866
Valeryia Bessonava <leranetwork@gmail.com>
2025-09-04 14:57:22 +0200

    chore: remove files (closes #12)

--- app/(protected)/history/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function History() {
-  return (
-    <div style={{}}>
-      <h1>History</h1>
-    </div>
-  );
-}
--- app/(protected)/variables/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function VariablesPage() {
-  return (
-    <div style={{}}>
-      <h1>Variables</h1>
-    </div>
-  );
-}
