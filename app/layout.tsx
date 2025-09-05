3209c887e08c950087377533465546fcfd0b6c7d
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-09-05 17:59:06 +0300

    feat: integrate header in layout and app/page.tsx(#15)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import { ReactNode } from "react";
 
+import { Header } from "@app/_components";
+
+import "@/shared/styles/globals.css";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-bg-primary text-base">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicLayout() {
+  return (
+    <div style={{}}>
+      <h1 className="text-red-500">Public Page</h1>
+    </div>
+  );
+}
