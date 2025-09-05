47448bb7021914a6abc3644608a77e719a4b7623
whowouldwin <leranetwork@gmail.com>
2025-09-05 23:50:52 +0300

    feat: add Firebase authentication and Firestore integration (#24)

--- app/[locale]/(auth)/sign-up/page.tsx
+++ app/[locale]/(auth)/sign-up/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import EmailSignUpForm from "@app/(auth)/sign-up/_components/sign-up-form";
+
 export default function SignUpPage() {
-  return <div>Sign Up</div>;
+  return (
+    <div className="mx-auto max-w-sm space-y-3">
+      <h1 className="text-lg font-semibold">Sign up</h1>
+      <EmailSignUpForm />
+    </div>
+  );
 }
