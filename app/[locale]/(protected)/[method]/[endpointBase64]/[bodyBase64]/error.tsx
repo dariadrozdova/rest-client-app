8a69e59fa7286689c0e7fcbff08e6b28133577d2
whowouldwin <leranetwork@gmail.com>
2025-09-08 12:39:41 +0200

    fix: add default export React component to error.tsx (closes #24)

--- app/[locale]/(protected)/[method]/[endpointBase64]/[bodyBase64]/error.tsx
+++ app/[locale]/(protected)/[method]/[endpointBase64]/[bodyBase64]/error.tsx
@@ -1,1 +1,22 @@
 "use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div className="p-4 text-red-600">
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button
+        className="mt-2 rounded bg-red-500 px-4 py-2 text-white"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
