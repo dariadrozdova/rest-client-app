d0aee2b3651e3721aae960d016a5df29d10a35e6
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-09-09 17:56:44 +0300

    feat: add new reducer in the store(#34)

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import headersReducer from "@/store/slices/header-slice";
 import tabReducer from "@/store/slices/tab-open-slice";
 
 export const store = configureStore({
   reducer: {
     tabs: tabReducer,
+    headers: headersReducer,
   },
 });
 
