e267e93f8a601c569e885c78cef6d7daf3499ffa
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-09-09 13:58:51 +0300

    feat: develop mock tabs to check state update(#32)

--- app/[locale]/(protected)/_components/body-editor.tsx
+++ app/[locale]/(protected)/_components/body-editor.tsx
@@ -1,1 +1,9 @@
-//Request body editor / JSON viewer… support prettifying; JSON & plain text
+import { useSelector } from "react-redux";
+
+import { RootState } from "@store/store";
+
+export function BodyEditor() {
+  const activeTab = useSelector((state: RootState) => state.tabs.activeTab);
+  const isHeadersOpen = activeTab === "body";
+  return isHeadersOpen && <div className="text-5xl">BODY</div>;
+}
