8c8398b7a66b1c0652bc9979c86b778e3683a20d
Daria Tkachenko <dariadrozdova10@gmail.com>
2025-09-10 00:08:52 +0100

    feat: add body editor tab content (#36)

--- shared/lib/i18n/messages/ru.index.ts
+++ shared/lib/i18n/messages/ru.index.ts
@@ -1,3 +1,4 @@
+import bodyEditor from "@shared/lib/i18n/messages/ru/body-editor.json";
 import errors from "@shared/lib/i18n/messages/ru/errors.json";
 import footer from "@shared/lib/i18n/messages/ru/footer.json";
 import header from "@shared/lib/i18n/messages/ru/header.json";
@@ -8,4 +9,5 @@ export default {
   footer,
   header,
   "protected-header": protectedHeader,
+  "body-editor": bodyEditor,
 } as const;
