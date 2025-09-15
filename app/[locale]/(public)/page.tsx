5554f4ced68c8a93594446c3f7b512cd0fbdd138
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-09-15 21:20:18 +0300

    feat: integrate client list into the main page(#18)

--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,16 +1,19 @@
-import { ThreeDScene } from "@app/[locale]/(public)/components/three-d-scene";
+import { getTranslations } from "next-intl/server";
+
+import { LogosShowcase } from "@app/[locale]/(public)/components";
+import { ThreeDScene } from "@app/[locale]/(public)/components";
 
 import { LAYERS } from "@/shared/globals";
 
-export default function PublicLayout() {
+export default async function PublicLayout() {
+  const t = await getTranslations("main-page");
   return (
     <>
       <section className="relative min-h-[900px] overflow-hidden">
         <div className="relative z-10 mx-auto px-8 pt-20">
-          <h1 className="w-3/5 text-7xl font-bold">Test. Debug. Repeat.</h1>
+          <h1 className="w-4/5 text-7xl font-bold">{t("slogan")}</h1>
           <h3 className="text-text-secondary mt-4 w-1/2 text-2xl font-medium">
-            Meet the REST client built for developers who move fast. Streamline
-            API testing, debugging, and collaboration.
+            {t("headline")}
           </h3>
         </div>
         <div className="pointer-events-none absolute inset-0 z-0 mt-28 [height:100%] [mask-image:linear-gradient(0deg,transparent_0%,transparent_20%,white_40%,white_70%,transparent_100%)] [--h:900] [--k:calc(min(100vw/(var(--w)*1px),100dvh/(var(--h)*1px)))] [--w:1440] [perspective-origin:50%_40%] [perspective:calc(var(--k)*1000px)]">
@@ -19,7 +22,7 @@ export default function PublicLayout() {
           </div>
         </div>
       </section>
-      <h2 className="my-8 text-5xl">Pricing</h2>
+      <LogosShowcase />
     </>
   );
 }
