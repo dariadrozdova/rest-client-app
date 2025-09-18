ba334bb0a81936414190ae730ea6cbc046bf16af
Daria Tkachenko <dariadrozdova10@gmail.com>
2025-09-18 23:46:17 +0100

    fix: fix console warnings

--- app/[locale]/(auth)/_components/modal.tsx
+++ app/[locale]/(auth)/_components/modal.tsx
@@ -12,7 +12,7 @@ interface ModalProps {
 export default function Modal({ onClose, children }: ModalProps) {
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-md"
+      className="absolute inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-md"
       onMouseDown={onClose}
     >
       <div
